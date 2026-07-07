d3a4116afda0de5922f87891771afefd89d310f6
Milo Tek <milotek@google.com>
2026-07-07 10:08:19 +0100

    fix(copyparty): replace --xff-src with --rproxy 1

--- server-modules/copyparty.nix
+++ server-modules/copyparty.nix
@@ -12,7 +12,6 @@
     exec ${pkgs.copyparty}/bin/copyparty \
       -i 127.0.0.1 \
       -p 3923 \
-      --xff-src 127.0.0.1 \
       --rproxy 1 \
       -a milotek:"$pw" \
       -v /var/lib/copyparty:files:r:rwmd,milotek
